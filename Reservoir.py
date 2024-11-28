af08ef226caab7910f8fff495471379e21d7aa06
Jonathan Dong <jonathan.dong@epfl.ch>
2024-11-28 21:20:10 +0100

    Try a first parallelized version (to debug)

--- Reservoir.py
+++ Reservoir.py
@@ -2,10 +2,10 @@ import numpy as np
 import torch
 
 use_cuda = torch.cuda.is_available()
-default_device = torch.device("cuda" if use_cuda else "cpu")
+default_device = torch.device("cuda:2" if use_cuda else "cpu")
 
 
-class Reservoir(torch.nn.Module):
+class CustomReservoir(torch.nn.Module):
     """
     Implements a minimal Reservoir Computing algorithm for stability analysis.
     :param input_size: dimension of the input
@@ -21,7 +21,7 @@ class Reservoir(torch.nn.Module):
                  W_res=None, W_in=None,
                  f='tanh',
                  seed=None, device=default_device):
-        super(Reservoir, self).__init__()
+        super().__init__()
 
         # Parameter initialization
         self.input_size = input_size
@@ -77,19 +77,48 @@ class Reservoir(torch.nn.Module):
                     self.input_scale * self.W_in @ input_data[i, :] +
                     self.res_scale * self.W_res @ states[i, :]
                 ) / np.sqrt(self.res_size)
+        return states
+    
+    def forward_parallel(self, input_data, res_scale_list, initial_state=None):
+        """
+        Compute the reservoir states for the given sequence
+        :param input_data: input sequence of shape (seq_len, input_size)
+        :param initial_state: initial reservoir state at t=0 (res_size, )
+        :return: successive reservoir states (seq_len+1, res_size)
+        """
+        n_res_scale = len(res_scale_list)
+        input_data = input_data.to(self.device)
+        seq_len = input_data.shape[0]
+        states = torch.zeros((n_res_scale, seq_len+1, self.res_size)).to(self.device)  
+        # will contain the reservoir states
+        if initial_state is not None:
+            states[0, :] = initial_state
 
+        res_scale_tensor = torch.tensor(res_scale_list).to(self.device).unsqueeze(1)
+        for i in range(seq_len):
+            input_contribution = self.input_scale * self.W_in @ input_data[i, :]
+            input_contribution = input_contribution.repeat(n_res_scale, 1)
+            res_contributions = res_scale_tensor * (states[:, i, :] @ self.W_res)
+            states[:, i+1, :] = self.f(
+                    input_contribution +
+                    res_contributions
+                ) / np.sqrt(self.res_size)
         return states
         
-    def stability_test(self, input_data):
+    def stability_test(self, input_data, res_scale_list, initial_state1=None, initial_state2=None):
         """
         Stability test: Iterate the reservoir for the same input data and two different initial state
         Follows the distance between the reservoir states through time, whether they converge to the same trajectory
         """
-        
-        initial_state1 = torch.randn(self.res_size).to(self.device) / np.sqrt(self.res_size)
-        initial_state1 = initial_state1 / torch.norm(initial_state1)
-        initial_state2 = torch.randn(self.res_size).to(self.device) / np.sqrt(self.res_size)
-        initial_state2 = initial_state2 / torch.norm(initial_state2)
-        states1 = self.forward(input_data,initial_state1)
-        states2 = self.forward(input_data,initial_state2)
-        return torch.sum((states1 - states2)**2, dim=1)
+        n_res_scale = len(res_scale_list)
+        if initial_state1 is None:
+            initial_state1 = torch.randn(self.res_size).to(self.device) / np.sqrt(self.res_size)
+            initial_state1 = initial_state1 / torch.norm(initial_state1)
+            initial_state1 = initial_state1.repeat(n_res_scale, 1)
+        if initial_state2 is None:
+            initial_state2 = torch.randn(self.res_size).to(self.device) / np.sqrt(self.res_size)
+            initial_state2 = initial_state2 / torch.norm(initial_state2)
+            initial_state2 = initial_state2.repeat(n_res_scale, 1)
+        states1 = self.forward_parallel(input_data, res_scale_list, initial_state1)
+        states2 = self.forward_parallel(input_data, res_scale_list, initial_state2)
+        return torch.sum((states1 - states2)**2, dim=2)
