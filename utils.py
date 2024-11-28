31ac1eb2cffb5fe49766f2e0c41448e16db4852a
AleDinve <giuseppealessio.d@student.unisi.it>
2024-11-28 22:12:14 +0100

    erf frontier

---
+++ utils.py
@@ -0,0 +1,5 @@
+import numpy as np
+import torch
+
+def erf_frontier(res_scale):
+    return 4*res_scale**2/np.pi - 1/4 - 2*res_scale**2/np.pi*np.arcsin((16*res_scale**4-np.pi**2)/(16*res_scale**4+np.pi**2))
