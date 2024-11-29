8b494922e936d62a644193ad2cf36272b39595dc
AleDinve <giuseppealessio.d@student.unisi.it>
2024-11-29 14:44:53 +0100

    notebook and utils updated

--- utils.py
+++ utils.py
@@ -2,4 +2,4 @@ import numpy as np
 import torch
 
 def erf_frontier(res_scale):
-    return 4*res_scale**2/np.pi - 1 - 2*res_scale**2/np.pi*np.arcsin((16*res_scale**4-np.pi**2)/(16*res_scale**4+np.pi**2))
+    return np.sqrt(4*res_scale**4/np.pi**2 - 1/(4) - 2*res_scale**2/np.pi*np.arcsin((16*res_scale**4-np.pi**2)/(16*res_scale**4+np.pi**2))+1e-6)
